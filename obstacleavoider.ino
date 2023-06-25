4f5c51e1f4f3b7068719bb8a6c3363932ac7bff0
ankush07das <dasankushad123@gmail.com>
2023-06-25 13:43:16 +0530

    code uploaded

---
+++ obstacleavoider.ino
@@ -0,0 +1,208 @@
+#include <Servo.h>          //Servo motor library. This is standard library //Robot Lk
+#include <NewPing.h>        //Ultrasonic sensor function library. You must install this library //Robot Lk
+//Robot Lk YouTube Channel-https://www.youtube.com/c/RobotLk
+//our L298N control pins
+const int LeftMotorForward = 7;
+const int LeftMotorBackward = 13;
+const int RightMotorForward = 3;
+const int RightMotorBackward = 4;
+const int ENA = 5;
+const int ENB = 6;
+#define Maxspeed 200
+//LED Robot Lk
+const int ForwardLED = 8;
+const int BackwardLED = 9;
+const int LeftLED = 11;
+const int RightLED = 12;
+
+//sensor pins
+#define trig_pin A1 //analog input 1
+#define echo_pin A2 //analog input 2
+
+#define maximum_distance 200
+boolean goesForward = false;
+int distance = 100;
+
+NewPing sonar(trig_pin, echo_pin, maximum_distance); //sensor function
+Servo servo_motor; //our servo name
+
+//Robot Lk
+void setup(){
+
+  pinMode(RightMotorForward, OUTPUT);
+  pinMode(LeftMotorForward, OUTPUT);
+  pinMode(RightMotorBackward, OUTPUT);
+  pinMode(LeftMotorBackward, OUTPUT);
+  pinMode(ForwardLED, OUTPUT);//Robot Lk
+  pinMode(BackwardLED, OUTPUT);//Robot Lk
+  pinMode(LeftLED, OUTPUT);//Robot Lk
+  pinMode( RightLED, OUTPUT);
+  pinMode(ENA,OUTPUT);
+  pinMode(ENB,OUTPUT);
+  servo_motor.attach(10); //our servo pin
+
+  servo_motor.write(115);
+  delay(2000);
+  distance = readPing();
+  delay(100);
+  distance = readPing();
+  delay(100);
+  distance = readPing();
+  delay(100);
+  distance = readPing();
+  delay(100);
+}
+
+void loop(){
+
+  int distanceRight = 0;
+  int distanceLeft = 0;
+  delay(50);
+
+  if (distance <= 45){
+    moveStop();
+    delay(300);
+    moveBackward();
+    delay(400);
+    moveStop();
+    delay(300);
+    distanceRight = lookRight();
+    delay(300);
+    distanceLeft = lookLeft();
+    delay(300);
+
+    if (distance >= distanceLeft){
+      turnRight();
+      moveStop();
+    }
+    else{
+      turnLeft();
+      moveStop();
+    }
+  }
+  else{
+    moveForward(); 
+  }
+    distance = readPing();
+}
+
+int lookRight(){  
+
+   
+  servo_motor.write(50);
+  delay(500);
+  int distance = readPing();
+  delay(100);
+  servo_motor.write(115);
+  return distance;
+}
+
+int lookLeft(){
+
+  servo_motor.write(170);
+  delay(200);
+  int distance = readPing();
+  delay(100);
+  servo_motor.write(115);
+  return distance;
+  delay(100);
+}
+
+int readPing(){
+  delay(70);
+  int cm = sonar.ping_cm();
+  if (cm==0){
+    cm=250;
+  }
+  return cm;
+}
+
+void moveStop(){
+  
+  digitalWrite(RightMotorForward, LOW);
+  digitalWrite(LeftMotorForward, LOW);
+  digitalWrite(RightMotorBackward, LOW);
+  digitalWrite(LeftMotorBackward, LOW);
+   digitalWrite(ForwardLED, LOW);
+  digitalWrite(BackwardLED, LOW);
+  digitalWrite(LeftLED, LOW);
+  digitalWrite(RightLED, LOW);
+}
+
+void moveForward(){
+
+  if(!goesForward){
+
+    goesForward=true;
+    
+    digitalWrite(LeftMotorForward, HIGH);
+    digitalWrite(RightMotorForward, HIGH);
+  
+    digitalWrite(LeftMotorBackward, LOW);
+    digitalWrite(RightMotorBackward, LOW);
+
+     analogWrite(ENA,Maxspeed);
+     analogWrite(ENB,Maxspeed);
+
+    digitalWrite(ForwardLED, HIGH);//Robot Lk//Robot Lk
+    
+  }
+}
+
+void moveBackward(){
+
+  goesForward=false;
+
+  digitalWrite(LeftMotorBackward, HIGH);
+  digitalWrite(RightMotorBackward, HIGH);
+  
+  digitalWrite(LeftMotorForward, LOW);
+  digitalWrite(RightMotorForward, LOW);
+
+ digitalWrite(BackwardLED, HIGH);//Robot Lk
+ 
+}
+
+
+void turnRight(){
+
+  digitalWrite(RightLED, HIGH);
+  
+ 
+
+  digitalWrite(LeftMotorForward, HIGH);
+  digitalWrite(RightMotorBackward, HIGH);
+  
+  digitalWrite(LeftMotorBackward, LOW);
+  digitalWrite(RightMotorForward, LOW);
+  
+  delay(250);
+  
+  digitalWrite(LeftMotorForward, HIGH);
+  digitalWrite(RightMotorForward, HIGH);
+  
+  digitalWrite(LeftMotorBackward, LOW);
+  digitalWrite(RightMotorBackward, LOW);
+ 
+  
+  
+}
+
+void turnLeft(){
+
+  digitalWrite(LeftLED, HIGH);
+
+  digitalWrite(LeftMotorBackward, HIGH);
+  digitalWrite(RightMotorForward, HIGH);
+  
+  digitalWrite(LeftMotorForward, LOW);
+  digitalWrite(RightMotorBackward, LOW);
+
+  delay(250);
+  
+  digitalWrite(LeftMotorForward, HIGH);
+  digitalWrite(RightMotorForward, HIGH);
+  
+  digitalWrite(LeftMotorBackward, LOW);
+  digitalWrite(RightMotorBackward, LOW);
+}
